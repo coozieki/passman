13062e1c4a81b84a4b687449a09dac7858264687
coozieki <vadimvor.work@gmail.com>
2022-07-26 19:50:59 +0300

    Don't show password directly, instead copy it to clipboard

--- internal/renderer/main.go
+++ internal/renderer/main.go
@@ -2,7 +2,10 @@ package renderer
 
 import (
 	"fmt"
+	"golang.design/x/clipboard"
+	"log"
 	"passman/internal/interfaces"
+	"strconv"
 )
 
 type consoleRenderer struct {
@@ -14,9 +17,34 @@ func (r *consoleRenderer) Render(records []interfaces.Record) {
 		fmt.Println("ID: ", i+1)
 		fmt.Println("Name: ", record.Name)
 		fmt.Println("Login: ", record.Login)
-		fmt.Println("Password: ", record.Password)
 		fmt.Println("Description: ", record.Description)
 		fmt.Println("-----------------------------------")
+	}
+
+	err := clipboard.Init()
+	if err != nil {
+		log.Fatal("program failed unexpectedly")
+	}
+
+	fmt.Print("Copy password to clipboard by typing ID: ")
+
+	var chosenID string
+
+	_, err = fmt.Scanln(&chosenID)
+	if err != nil {
+		log.Fatal("ID must be numeric and existent")
+	}
+
+	chosenIDToInt, err := strconv.Atoi(chosenID)
+	if err != nil || chosenIDToInt < 1 || chosenIDToInt > len(records) {
+		log.Fatal("ID must be numeric and existent")
+	}
+
+	fmt.Println("Password copied to clipboard")
+	changed := clipboard.Write(clipboard.FmtText, []byte(records[chosenIDToInt-1].Password))
+	select {
+	case <-changed:
+		fmt.Println(`Password is no longer available from clipboard.`)
 	}
 }
 
